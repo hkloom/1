7ac0548858838d62b6ee5a05d31992c16440af1d
Tai Rodrig <taicapra@gmail.com>
2014-04-15 15:39:33 -0700

    got connection to mongolab working

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 var express = require('express');
-var logfmt = require("logfmt");
+var logfmt = require('logfmt');
+var mongo = require('mongodb');
+var Server = mongo.Server;
+var Db = mongo.Db;
 var app = express();
+
+var server = new Server('ds047387.mongolab.com', 47387, {auto_reconnect : true});
+var db = new Db('heroku_app24135520',server);
+
+db.open(function(err,client){
+	client.authenticate('heroku_app24135520','osoke5ha3189i78benpm0r6l2c',function(err,success){
+		var subs = db.collection("subs");
+		console.log(subs);
+		subs.insert({"test":"boobs"},{w: 1},function(err,records){
+			console.log("Record added: "+records[0]._id);
+		});
+	});
+});
 
 app.use(logfmt.requestLogger());
 app.engine('html', require('ejs').renderFile);
@@ -15,7 +31,7 @@ app.get('/challenge/:number', function(req,res){
   	});
 });
 
-var port = Number(process.env.PORT || 5000);
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
