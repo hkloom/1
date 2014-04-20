bf60a2c9f4a0caf4ef5177dac999ec61fd51ed1a
Tai Rodrig <taicapra@gmail.com>
2014-04-19 18:52:11 -0700

    tryna post

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(logfmt.requestLogger());
 app.engine('html', require('ejs').renderFile);
 app.get('/challenge/:number', function(req,res){
+    console.log("on index page");
   	var num =  req.params.number;
   	df.find({test: "shoe"},function(items){
   		res.render("index.html",
@@ -21,17 +22,12 @@ app.get('/challenge/:number', function(req,res){
   	});
   	
 });
-app.get('/edit', function(req,res){
-    var num =  req.params.number;
-    df.find({test: "shoe"},function(items){
-      res.render("index.html",
+app.post('/edit', function(req,res){
+  console.log("redirecting to edit page");
+      res.render("system.html",
         { 
-          title: num+" -- "+num+" -- "+num+" -- "+num+" -- "+num,
-          items: items
-        }
-      );
-    });
-    
+          title: "test"
+        }); 
 });
 
 var port = process.env.PORT || 5000;
