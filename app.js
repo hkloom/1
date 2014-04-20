d4f88a6d4169c3fc312dbb3aafd988a4e0539d1d
Tai Rodrig <taicapra@gmail.com>
2014-04-19 18:08:01 -0700

    got clicking to work

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ app.get('/challenge/:number', function(req,res){
   	});
   	
 });
+app.get('/edit', function(req,res){
+    var num =  req.params.number;
+    df.find({test: "shoe"},function(items){
+      res.render("index.html",
+        { 
+          title: num+" -- "+num+" -- "+num+" -- "+num+" -- "+num,
+          items: items
+        }
+      );
+    });
+    
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
