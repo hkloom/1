b0675f728ea6cb1a89511db2a17f90ff0371dea8
Tai Rodrig <taicapra@gmail.com>
2014-04-20 02:01:25 -0700

    redirects now kinda working

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 var express = require('express');
 var logfmt = require('logfmt');
 var df = require('./public/js/dbface.js');
+var bp = require('body-parser');
+var $ = require('jquery');
 var app = express();
 
 df.open();
 
-
 app.use(express.static(__dirname + '/public'));
 app.use(logfmt.requestLogger());
 app.engine('html', require('ejs').renderFile);
-app.get('/challenge/:number', function(req,res){
+app.use(bp());
+app.route('/')
+.get( function(req,res){
     console.log("on index page");
-  	var num =  req.params.number;
   	df.find({test: "shoe"},function(items){
   		res.render("index.html",
   			{	
-  				title: num+" -- "+num+" -- "+num+" -- "+num+" -- "+num,
+  				title: "VIEW",
   				items: items
   			}
   		);
-  	});
-  	
+  	});	
 });
-app.post('/edit', function(req,res){
-  console.log("redirecting to edit page");
+
+app.route('/edit')
+.post( function(req,res){
+    console.log("on edit page");
       res.render("system.html",
         { 
-          title: "test"
-        }); 
-});
+          title: "EDIT",
+          system: {test:"baby"}
+        }
+      );
+    });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
